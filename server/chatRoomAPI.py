21e294389973ebdcd7440caffd2577f959aa334b
DipeshMohanty1098 <60932129+DipeshMohanty1098@users.noreply.github.com>
2021-02-22 19:02:51 +0530

    Update chatRoomAPI.py

--- server/chatRoomAPI.py
+++ server/chatRoomAPI.py
@@ -3,13 +3,14 @@ from flask_restful import Api, Resource, reqparse, abort, marshal_with, fields
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-
+#setup
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chatRooms.db'
 db = SQLAlchemy(app)
 
+#make the data serializable
 chat_room = reqparse.RequestParser()
 chat_room.add_argument("chatRoomName", type=str, help="Chat Room Name")
 
@@ -18,19 +19,22 @@ resource_fields = {
     'chatRoomName': fields.String
 }
 
+#creating the main DB
 class ChatRoomModel(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     chatRoomName = db.Column(db.String(200), nullable = False)
 
-
+#db.create_all() (run only for the first time, to create the db)
 
 class ChatRoom(Resource):
-
+    
+    #get all the records in db
     @marshal_with(resource_fields)
     def get(self):
         chat_rooms = ChatRoomModel.query.all()
         return chat_rooms, 200
-
+    
+    #create a nw record
     @marshal_with(resource_fields)
     def post(self):
         args = chat_room.parse_args()
@@ -42,6 +46,7 @@ class ChatRoom(Resource):
         db.session.commit()
         return chat_room_name, 201
 
+#add route
 api.add_resource(ChatRoom, "/chatRooms/")
 
 if __name__ == "__main__":
